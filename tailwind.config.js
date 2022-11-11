b828f8ac29b24b1a1557451a9194a020fd6081f2
JoseAl05 <jparancibialinker@gmail.com>
2022-11-11 19:09:31 -0300

    minor change

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     './public/**/*.{html,css,js}',
     './src/**/*.{html,js,css}',
-    './dist/*.{html,js,css}'
+    './dist/*.{html,js,css}',
+    './index.html',
   ],
   theme: {
     extend: {},
