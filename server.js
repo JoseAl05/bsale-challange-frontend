3bb42df3f255925a4419dfd522ea14779f2fbb06
JoseAl05 <jparancibialinker@gmail.com>
2022-11-11 19:09:23 -0300

    minor change to express static middleware

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 
-app.use("/src", express.static(path.join(__dirname,"src")));
-app.use("/public", express.static(path.join(__dirname,"public")));
+
 app.use("/dist",express.static(path.join(__dirname,"dist")));
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
