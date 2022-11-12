615cd31539fcb29ebbf696e929e0ee8a550b91aa
JoseAl05 <jparancibialinker@gmail.com>
2022-11-11 23:02:37 -0300

    change index to public folder

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use("/dist",express.static(path.join(__dirname,'..',"dist")));
 app.get('/*', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
-  res.sendFile(path.resolve(__dirname,'..' ,'index.html'));
+  res.sendFile(path.resolve(__dirname,'public','index.html'));
 });
 
 app.listen(port);
