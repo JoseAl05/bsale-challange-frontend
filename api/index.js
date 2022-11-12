bb2260a586d02d4ca315a90abe2b48f770c46882
JoseAl05 <jparancibialinker@gmail.com>
2022-11-11 22:55:38 -0300

    minor changes for deploy

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use("/dist",express.static(path.join(__dirname,"dist")));
 app.get('/*', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
