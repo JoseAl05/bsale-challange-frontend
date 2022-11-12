31513fed7d56651bc247918dafc6bcea51880e97
JoseAl05 <jparancibialinker@gmail.com>
2022-11-12 17:47:07 -0300

    fix server

--- api/index.js
+++ api/index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const port = process.env.PORT || 4000;
 const app = express();
 
-
-
-app.use("/dist",express.static(path.join(__dirname,"public","dist")));
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+app.use("/dist", express.static(path.join(__dirname, "..", "public", "dist")));
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "..", "public") });
 });
 
 app.listen(port);
 
-module.exports = app
+module.exports = app;
